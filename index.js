0102d495466a960c7997dc6d166fa95b498ca9a6
Maeve <mariya.kiryakulova@gmail.com>
2023-03-07 00:43:38 +0000

    add-questions
    Added the rest of the questions

--- index.js
+++ index.js
@@ -10,11 +10,58 @@ inquirer.prompt([
         message: "Name of your repo:",
         name: "title"
     },
+    {
+        type: "input",
+        message: "Description of your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation instructions:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage information:",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "License:",
+        name: "license",
+        choices: [
+            "MIT",
+            "Apache 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "Contribution guidelines:",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Test instructions:",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Your GitHub username:",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Your email address:",
+        name: "email"
+    }
 ])
 // function to write README file
 .then((response) => {
     const markdown = generateMarkdown(response);
-    
+
     // function to initialize program
     fs.writeFile("CreatedREADME.md", markdown, (error) => {
     error ? console.log(error) : console.log("README generated!");

fbd33c818de8b382474039bc65e83f5f4ce1ff56
Maeve <mariya.kiryakulova@gmail.com>
2023-03-07 00:41:52 +0000

    add-licence-bedges
    Using ES6 conditional operator added conditions so licence budges changes depending on what the user chose

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 // function to generate markdown for README
 function generateMarkdown(response) {
-  return `
+  return `${`
   # ${response.title}
   ## Description
   ${response.description}
@@ -26,6 +26,12 @@ function generateMarkdown(response) {
   ${response.test}
   
   ## License
+  `
+  + (response.license === "MIT" ? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' : "") +
+  (response.license === "Apache 2.0" ? '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' : "") +
+  (response.license === "GPL 3.0" ? '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' : "") +
+  (response.license === "BSD 3" ? '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)' : "") +
+  (response.license === "None" ? "None" : "")}
   
   ## Questions
   For any questions or feedback, please contact me at [GitHub](https://github.com/${response.github}) or Email: ${response.email}
